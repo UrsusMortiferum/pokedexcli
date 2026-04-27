b936638143d1b4b55a91324cecaa0bbc15ebf53f
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2026-04-27 14:29:42 +0200

    Basic REPL setup

--- repl.go
+++ repl.go
@@ -1,7 +1,28 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		output := words[0]
+
+		fmt.Printf("You command was: %s\n", output)
+	}
+
+}
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
 }
