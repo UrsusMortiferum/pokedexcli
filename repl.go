e2c9a76da71da72c86f8de290b8f5fb16abf711d
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2026-04-27 15:29:11 +0200

    Added command support in REPL (included help and exit so far)

--- repl.go
+++ repl.go
@@ -17,12 +17,43 @@ func startRepl() {
 			continue
 		}
 
-		output := words[0]
+		commandName := words[0]
 
-		fmt.Printf("You command was: %s\n", output)
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
-
 }
+
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
